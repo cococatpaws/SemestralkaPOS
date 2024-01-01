5da846820ee08822161d52b102cb1a45ac259922
Luna575 <nikarybanska01@gmail.com>
2024-01-01 21:38:44 +0100

    vzor strktura a metody na vypis

--- main.cpp
+++ main.cpp
@@ -4,9 +4,84 @@
 #include <queue>
 #include <random>
 #include <string>
+#include <algorithm>
 #include "my_socket.h"
+using namespace std;
+
+// change row and column value to set the canvas size
+const int row = 5;
+const int col = 4;
+
+
+struct Vzor {
+    string vzor;
+    static Vzor vytvor(int **vzor);
+    };
+
+Vzor Vzor::vytvor(int **vzor) {
+    string txt;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+           txt+= to_string(vzor[i][j] )+ ";";
+        }
+    }
+    Vzor v = {txt};
+    return v;
+}
+
+ void generujSpetne(string txt, int ** vzor){
+    txt.erase(std::remove(txt.begin(), txt.end(), ';'), txt.end());
+    const char* pomocna = txt.c_str();
+    int pocet = 0;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            vzor[i][j] = pomocna[pocet] - '0';
+            pocet++;
+        }
+    }
+
+}
+
+void row_line() {
+    cout << endl;
+    for (int i = 0; i < col; i++) {
+        cout << " -----";
+    }
+    cout << endl;
+}
+void vypis(int** vzor) {
+    row_line();
+    for (int i = 0; i < row; i++) {
+        cout << ":";
+        for (int j = 0; j < col; j++) {
+            cout << "  " << vzor[i][j] << "  :";
+        }
+        row_line();
+    }
+}
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int** txt = new int*[row];
+    for (int i = 0; i < row; i++) {
+        txt[i] = new int[col];
+    }
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            txt[i][j] = 1;
+        }
+    }
+
+    Vzor vzor = Vzor::vytvor(txt);
+    cout << vzor.vzor;
+
+    generujSpetne(vzor.vzor, txt);
+    vypis(txt);
+    for (int i = 0; i < row; i++) {
+        delete[] txt[i];
+    }
+    delete[] txt;
+
 }
